ff0c70fff7295bd20c5b4a453680553f11bbb603
Yaroslavhavryliuk <gv_yarik@ukr.net>
2021-06-14 15:54:37 +0300

    ProfileMenu Documentation

--- Films for you/profilemenu.h
+++ Films for you/profilemenu.h
@@ -9,19 +9,29 @@ namespace Ui {
 class ProfileMenu;
 }
 
+/*!
+ * \brief The ProfileMenu class
+ * Window for work with profiles
+ */
 class ProfileMenu : public QWidget
 {
     Q_OBJECT
 
 public:
+    /*!
+     * \brief ProfileMenu
+     * \param parent
+     * Constructor
+     */
     explicit ProfileMenu(QWidget *parent = nullptr);
+    /*!
+     * Destructor
+     */
     ~ProfileMenu();
 
 private slots:
     void on_addProfile_clicked();
-
     void on_deleteProfile_clicked();
-
     void on_SelectProfile_clicked();
 
 signals: void sendProfile(Profile profile);
